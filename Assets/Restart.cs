b72148e1ec36818b7b63739d5bcec2c9be68139d
Samir Mohamed <samirmohamedcs@gmail.com>
2015-10-27 13:59:31 -0400

    The WHole thing yoh

---
+++ Assets/Restart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+		   {
+			
+			Pathermaker.floorMakerCount = 1;
+			Pathermaker.totalTiles = 0;
+			Application.LoadLevel(0);
+		}
+	
+	}
+}
